ec8bc0a97940189f8562cf69e8969882159fdedb
delabere <jack.rickards@hotmail.co.uk>
2024-05-12 18:11:40 +0100

    update process.py

--- process.py
+++ process.py
@@ -1,6 +1,10 @@
-from PIL import Image
+#!/usr/bin/env python3
+import sys
 
-def convert_and_rotate(input_path, output_path):
+from PIL import Image
+from inky.auto import auto
+
+def convert_and_rotate(input_path):
     # Open an existing image
     img = Image.open(input_path)
     
@@ -18,8 +22,36 @@ def convert_and_rotate(input_path, output_path):
     # Rotate the image by 90 degrees
     rotated_img = img.rotate(90, expand=True)
 
+    return rotated_img
+
     # Save the resulting image
-    rotated_img.save(output_path, "PNG")
+    # rotated_img.save(output_path, "PNG")
+
+
+inky = auto(ask_user=True, verbose=True)
+saturation = 0.7
+
+# if len(sys.argv) == 1:
+#     print("""
+# Usage: {file} image-file
+# """.format(file=sys.argv[0]))
+#     sys.exit(1)
+
+# image = Image.open(sys.argv[1])
+image = convert_and_rotate("test.png")
+resizedimage = image.resize(inky.resolution)
+
+# if len(sys.argv) > 2:
+#     saturation = float(sys.argv[2])
+
+inky.set_image(resizedimage, saturation=saturation)
+inky.show()
+
+
 
 # Example usage
-convert_and_rotate('test.png', 'test_rotate.png')
+# convert_and_rotate('test.png', 'test_rotate.png')
+
+
+
+
